cf5c199453a7a1e34cb044ac51da6a7569e58aa1
snehdeeptambe <83006962+snehdeeptambe@users.noreply.github.com>
2021-05-29 21:28:09 +0530

    Initial commit of new math.py

---
+++ new math.py
@@ -0,0 +1,12 @@
+#add implemented
+def add(x,y):
+    pass
+#sub implemented
+def subtract(x,y):
+    pass
+#multiply implemented
+def multiply(x,y):
+    pass
+#divide implemented
+def divide(x,y):
+    pass
